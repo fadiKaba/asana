45de9f52eaee7b53ccdeca3b8316aa0139d29be2
fadiKaba <fadikaba9@gmail.com>
2021-07-09 00:30:35 +0200

    hotspot

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const searchResult = document.querySelector("#search-result");
 const searchResultUl = document.querySelector("#search-result ul");
 const searchBtn = document.querySelector('#search-btn');
 const locationBtn = document.querySelector('#location-btn');
+const hotspotNavBtn = document.querySelector('.hotspot-nav-btn')
 
 
 let data = [
@@ -50,7 +51,28 @@ let images = [
     {id: 15, src: "https://source.unsplash.com/random/600x800"},
     {id: 16, src: "https://source.unsplash.com/random/600x900"},
     {id: 17, src: "https://source.unsplash.com/random/700x800"},
-]
+];
+
+let images2 = [
+    {id: 1, src: "https://source.unsplash.com/random/400x550"},
+    {id: 2, src: "https://source.unsplash.com/random/400x650"},
+    {id: 3, src: "https://source.unsplash.com/random/400x750"},
+    {id: 4, src: "https://source.unsplash.com/random/400x850"},
+    {id: 5, src: "https://source.unsplash.com/random/500x650"},
+    {id: 6, src: "https://source.unsplash.com/random/500x750"},
+    {id: 7, src: "https://source.unsplash.com/random/500x850"},
+    {id: 8, src: "https://source.unsplash.com/random/500x950"},
+    {id: 9, src: "https://source.unsplash.com/random/300x450"},
+    {id: 10, src: "https://source.unsplash.com/random/300x450"},
+    {id: 11, src: "https://source.unsplash.com/random/300x550"},
+    {id: 12, src: "https://source.unsplash.com/random/300x650"},
+    {id: 13, src: "https://source.unsplash.com/random/300x750"},
+    {id: 14, src: "https://source.unsplash.com/random/600x750"},
+    {id: 15, src: "https://source.unsplash.com/random/600x850"},
+    {id: 16, src: "https://source.unsplash.com/random/600x950"},
+    {id: 17, src: "https://source.unsplash.com/random/700x850"},
+];
+
 let result = [];
 
 // Search function
@@ -115,37 +137,88 @@ searchView.addEventListener('click', function(e){
 // End location btn
 
 // Search container
-function fillSearchContainer(arr){
+function fillSearchContainer(arr, tile){
     let searchCounter = 1;
     let column = '';
-    let mainCounter = 0;
-    document.querySelector('#search-column-2').innerHTML+= `<div class="search-page-title">find your taste</div>`;
+    document.querySelector('#search-column-2').innerHTML+= `<div class="search-page-title">${tile}</div>`;
     arr.forEach(item => {
         if(searchCounter < 3){
             column = document.querySelector('#search-column-' + searchCounter);
             column.innerHTML += 
             `
-            <div>
+            <div class="inner-div">
                 <a href="#"><img src="${item.src}" alt="${item.id}"></a>
             </div>
             `;
             searchCounter++;
-            mainCounter++;
         }else{
             column = document.querySelector('#search-column-' + searchCounter);
             column.innerHTML += 
             `
-            <div>
+            <div class="inner-div">
                 <a href="#"><img src="${item.src}" alt="${item.id}"></a>
             </div>
             `;
             searchCounter = 1;
-            mainCounter++;
         }
         
     });
 }
 
-fillSearchContainer(images);
+fillSearchContainer(images, 'find your test');
 
 // End search container
+
+// Search container 2
+function fillSearchContainerHotspot(arr, title){
+    let searchCounter = 1;
+    let column = '';
+    document.querySelector('#search-hotsopt-column-2').innerHTML+= `<div class="search-page-title-hotspot">${title}</div>`;
+    arr.forEach(item => {
+        if(searchCounter < 3){
+            column = document.querySelector('#search-hotsopt-column-' + searchCounter);
+            column.innerHTML += 
+            `
+            <div class="inner-div">
+                <a href="#"><img src="${item.src}" alt="${item.id}"></a>
+            </div>
+            `;
+            searchCounter++;
+        }else{
+            column = document.querySelector('#search-hotsopt-column-' + searchCounter);
+            column.innerHTML += 
+            `
+            <div class="inner-div">
+                <a href="#"><img src="${item.src}" alt="${item.id}"></a>
+            </div>
+            `;
+            searchCounter = 1;
+        }
+        
+    });
+}
+
+let isLoadedHotspot = false;
+hotspotNavBtn.addEventListener('click', function(){
+
+    if(!isLoadedHotspot){
+    fillSearchContainerHotspot(images2, `
+    <div>
+        <span class="small-dot"></span> 
+        <span class="small-dot"></span> 
+        <span class="small-dot"></span> 
+        <span class="small-dot"></span>
+        <span class="small-dot"></span>
+        <span class="small-dot"></span>
+        <span class="small-dot"></span>
+    </div>    
+        <span> wir kreiren</span>
+        <span> ERLEBNISE </span> 
+        `);
+        isLoadedHotspot = true;
+    }
+    
+});
+
+
+// End search container 2
